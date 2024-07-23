4d64e6b1b6d56bfe52d25101f51e58a1a3c88c56
lis56568 <lis56568@gmail.com>
2024-07-23 22:27:52 +0800

    加入功能及頁面

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -3,23 +3,23 @@ import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 
-window.Echo = new Echo({
-    broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
-    enabledTransports: ['ws', 'wss'],
-});
-
-
 // window.Echo = new Echo({
 //     broadcaster: 'reverb',
-//     key: 'bnxrlamr45txee3hryup',
-//     wsHost: 'ws.lis56568-service.com',
-//     wsPort: 80,
-//     wssPort: 443,
-//     forceTLS: 'https',
+//     key: import.meta.env.VITE_REVERB_APP_KEY,
+//     wsHost: import.meta.env.VITE_REVERB_HOST,
+//     wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+//     wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+//     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
 //     enabledTransports: ['ws', 'wss'],
 // });
+
+
+window.Echo = new Echo({
+    broadcaster: 'reverb',
+    key: 'bnxrlamr45txee3hryup',
+    wsHost: 'ws.lis56568-service.com',
+    wsPort: 80,
+    wssPort: 443,
+    forceTLS: 'https',
+    enabledTransports: ['ws', 'wss'],
+});
